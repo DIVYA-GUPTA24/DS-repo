9a1144339f35b66736c506292053db405919cc5f
DIVYA-GUPTA24 <114618524+DIVYA-GUPTA24@users.noreply.github.com>
2022-11-05 11:34:10 +0530

    Create decimal_to_hexadecimal.c

---
+++ Stack/decimal_to_hexadecimal.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define STACKSIZE 10
+#define true 1
+#define false 0
+
+struct stack
+{
+    int item[STACKSIZE];
+    int top;
+};
+struct stack s;
+void initialize()
+{
+    s.top = -1;
+}
+int isempty()
+{
+    if (s.top == -1)
+        return true;
+    else
+        return false;
+}
+void push(int x)
+{
+    if (s.top == STACKSIZE - 1)
+    {
+        printf("stack overflows");
+        exit(1);
+    }
+    s.top = s.top + 1;
+    s.item[s.top] = x;
+}
+int pop()
+{
+    int x;
+    if (isempty(s))
+    {
+        printf("stack underflows");
+        return 0;
+    }
+    else
+    {
+        x = s.item[s.top];
+        s.top = s.top - 1;
+        return x;
+    }
+}
+
+void decimaltohexadecimal(int y)
+{
+    int rem, a;
+    char conv[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    initialize();
+    while (y != 0)
+    {
+        rem = y % 16;
+        push(rem);
+        y = y / 16;
+    }
+    while (!isempty())
+    {
+        a = pop();
+        printf("%c", conv[a]);
+    }
+}
+int main()
+{
+    int y,x;
+    scanf("%d", &y);
+    decimaltohexadecimal(y);
+}
