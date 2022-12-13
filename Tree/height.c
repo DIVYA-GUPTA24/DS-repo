ebb35cb86254928cd5863289f3e961451cac2e65
DIVYA-GUPTA24 <114618524+DIVYA-GUPTA24@users.noreply.github.com>
2022-12-13 19:24:58 +0530

    Create height.c

---
+++ Tree/height.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+   char data;
+   struct node *left;
+   struct node *right;
+};
+
+struct node *makenode(char x)
+{
+   struct node *p;
+   p = (struct node*)malloc(sizeof(struct node));
+   p->data=x;
+   p->left=NULL;
+   p->right=NULL;
+   return p;
+}
+
+int preordertraversal(struct node *t)
+{
+    int count=0;
+     while(t!=NULL)
+     {
+         count++;
+    if(t!=NULL)
+    {
+
+        printf("%c ",t->data);
+        preordertraversal(t->left);
+        preordertraversal(t->right);
+    }
+    return count;
+    }
+}
+
+void inordertraversal (struct node *t)
+{
+
+    if(t!=NULL)
+    {
+
+        inordertraversal(t->left);
+        printf("%c ",t->data);
+        inordertraversal(t->right);
+    }
+}
+
+void postordertraversal(struct node *t)
+{
+
+    if(t!=NULL)
+    {
+        postordertraversal(t->left);
+        postordertraversal(t->right);
+        printf("%c ",t->data);
+
+    }
+}
+
+int max(int x,int y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+
+int height(struct node *t)
+{
+    if(t==NULL)
+        return 0;
+    else if(t->left==NULL && t->right==NULL)
+        return 0;
+    else
+        return max(height(t->left),height(t->right))+1;
+}
+
+void main()
+{
+    struct node *root;
+    root=NULL;
+    root=makenode('A');
+    root->left=makenode('B');
+    root->right=makenode('C');
+    root->left->left=makenode('D');
+    root->right->left=makenode('E');
+    root->right->right=makenode('F');
+    int h=height(root);
+    printf("the height of the tree is %d",h);
+
+}
