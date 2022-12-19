db88a0c8ad89c2640a2dc4537716d31d1d3a5ff0
DIVYA-GUPTA24 <114618524+DIVYA-GUPTA24@users.noreply.github.com>
2022-12-19 18:31:54 +0530

    Create Insert_BST.c

---
+++ Tree/Binary_search_tree/Insert_BST.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+   int data;
+   struct node *left;
+   struct node *right;
+};
+
+struct node *makenode(int x)
+{
+   struct node *p;
+   p = (struct node*)malloc(sizeof(struct node));
+   p->data=x;
+   p->left=NULL;
+   p->right=NULL;
+   return p;
+}
+void inordertraversal (struct node *t)
+{
+
+    if(t!=NULL)
+    {
+
+        inordertraversal(t->left);
+        printf("%d ",t->data);
+        inordertraversal(t->right);
+    }
+}
+int bstinsert(struct node **t,int x)
+{
+    struct node *p,*q;
+    p=*t;
+    q=NULL;
+    if(*t==NULL)
+    {
+        *t=makenode(x);
+    }
+    else
+    {
+    while(p!=NULL)
+    {
+         q=p;
+         if(x<p->data)
+         {
+             p=p->left;
+         }
+         else
+         {
+             p=p->right;
+         }
+    }
+    if(x<q->data)
+    {
+        q->left=makenode(x);
+    }
+    else{
+       q->right=makenode(x);
+    }
+    }
+}
+
+int main()
+{
+    struct node *root;
+    root=NULL;
+    bstinsert(&root,100);
+    bstinsert(&root,50);
+    bstinsert(&root,150);
+    bstinsert(&root,20);
+    bstinsert(&root,70);
+    bstinsert(&root,120);
+    bstinsert(&root,200);
+    bstinsert(&root,30);
+    bstinsert(&root,105);
+    bstinsert(&root,170);
+    bstinsert(&root,290);
+    bstinsert(&root,180);
+    printf("binary tree after insertion: ");
+    inordertraversal(root);
+}
