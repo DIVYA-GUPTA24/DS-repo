927a6ba33ee38b7b618f7fbeb826ac4bf0ad9181
DIVYA-GUPTA24 <114618524+DIVYA-GUPTA24@users.noreply.github.com>
2023-02-04 18:18:16 +0530

    Create Adj_list_with_weights.cpp

---
+++ Graph/Adj_list_with_weights.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int V,E;
+    cout<<"How many vertices are there?";
+    cin>>V;
+    cout<<"How many edges are there?";
+    cin>>E;
+
+    vector<pair<int,int>>adjlist[V+1];
+
+    for(int i=1;i<=E;i++)
+    {
+        int a,b,w;
+        cout<<"Enter the end points: ";
+        cin>>a>>b>>w;
+        adjlist[a].push_back({b,w});
+        adjlist[b].push_back({a,w});
+    }
+    for(int i=0;i<=V;i++)
+    {
+        cout<<i<<": ";
+        for(int j=0;j<adjlist[i].size();j++)
+        {
+            cout<<adjlist[i][j].first<<" "<<adjlist[i][j].second<<" ";
+        }
+        cout<<endl;
+    }
+
+}
