1513230cde29d6c03bff3c0cd8cba1b079223fe5
DIVYA-GUPTA24 <114618524+DIVYA-GUPTA24@users.noreply.github.com>
2023-02-07 19:47:03 +0530

    Create Upper_triangular_in_1-D_array.cpp

---
+++ Sparse Matrix/Upper_triangular_in_1-D_array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,a[10][10],c[10];
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+        int l=(n*(n+1))/2;
+        c[l]=a[i][j];
+        cout<<c[l]<<" ";
+        }
+    }
+}
